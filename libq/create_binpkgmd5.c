899a605233b6e71bc5ee1ef85d97329c38082993
Alberto Damo <alberto.damo@gmail.com>
2023-08-30 14:36:28 +0200

    bugfix file creation and error checking

--- libq/create_binpkgmd5.c
+++ libq/create_binpkgmd5.c
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include <fcntl.h>
+#include <err.h>
 
 #include "create_binpkgmd5.h"
 #include "safe_io.h"
@@ -13,7 +14,11 @@ void create_binpkgmd5_file(void *hash_to_copy_to_conv)
   int fd_to_create;
   char * hash_to_copy = (char *) hash_to_copy_to_conv;
 
-  fd_to_create= open("BINPKGMD5",660,O_WRONLY|O_CREAT);
+  fd_to_create= creat("BINPKGMD5",660);
+  if(fd_to_create<0){
+    warn("failed to create BINPKGMD5");
+    return ;
+  }
   safe_write(fd_to_create, hash_to_copy, HASH_SIZE);
   close(fd_to_create);
 }
